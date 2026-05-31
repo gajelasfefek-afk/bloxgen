3dbfa50a79f384f493f1c45f20015cac4b6bed42
gajelasfefek-afk <gajelasfefek@gmail.com>
2026-05-31 08:34:13 +0700

    Add Delta Executor loadstring loader

---
+++ loader.lua
@@ -0,0 +1,7 @@
+-- =============================================
+--   Roblox User ID Finder v2.4 - Delta Loader
+--   By: github.com/gajelasfefek-afk
+--   For: Delta Executor
+-- =============================================
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/gajelasfefek-afk/bloxgen/main/find.lua"))()
